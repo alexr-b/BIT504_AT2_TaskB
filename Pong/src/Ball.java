747fcbcdffe366179e39b497ea215d335df3bb53
alexr-b <alex.rob_brist@outlook.com>
2021-09-21 16:31:18 +1200

    Update of Text and Font
    Changed the Score and Winner text font family to something more
    appropriate
    
    Updated the Text line to "You Win!"

--- Pong/src/Ball.java
+++ Pong/src/Ball.java
@@ -23,7 +23,7 @@ import java.awt.Color;
            
            // Set initial position created by placing ball directly in middle of screen
            
-           setInitialPosition((panelWidth / 2) - (getWidth() / 2), (panelHeight / 2) - (getHeight() / 2));
+           setInitialPosition(panelWidth / 2 - (getWidth() / 2), panelHeight / 2 - (getHeight() / 2));
            resetToInitialPosition();
 
       }
--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -37,13 +37,13 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	  private final static int SCORE_TEXT_X = 100;
       private final static int SCORE_TEXT_Y = 100;
       private final static int SCORE_FONT_SIZE = 50;
-      private final static String SCORE_FONT_FAMILY = "Serif";
+      private final static String SCORE_FONT_FAMILY = "Forte";
       
       private final static int WINNER_TEXT_X = 200;
       private final static int WINNER_TEXT_Y = 200;
       private final static int WINNER_FONT_SIZE = 40;
-      private final static String WINNER_FONT_FAMILY = "Serif";
-      private final static String WINNER_TEXT = "WIN!";
+      private final static String WINNER_FONT_FAMILY = "Arial";
+      private final static String WINNER_TEXT = "YOU WIN!";
       
 	 
 	 
